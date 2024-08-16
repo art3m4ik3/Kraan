70554666839385d4f45f628756a4fdcd2aa6f532
art3m4ik3 <fritaimru@mail.ru>
2024-08-16 05:34:25 +0300

    Создание экрана MainScreen + создание BottomNavigationMenu

--- app/src/main/java/ru/art3m4ik3/kraan/presentation/pages/MainActivity.kt
+++ app/src/main/java/ru/art3m4ik3/kraan/presentation/pages/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.art3m4ik3.kraan.presentation.pages
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.art3m4ik3.kraan.R
 import ru.art3m4ik3.kraan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // TODO: get auth data from api
+        val authorized = false
+        if (authorized) {
+            binding.bottomNavigationView.menu.removeItem(R.id.navigation_login)
+        } else {
+            binding.bottomNavigationView.menu.removeItem(R.id.navigation_logout)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, MainScreenFragment())
+            .commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_main -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.fragmentContainer.id, MainScreenFragment())
+                        .commit()
+                }
+            }
+
+            true
+        }
     }
 }
---
+++ app/src/main/java/ru/art3m4ik3/kraan/presentation/pages/MainScreenFragment.kt
@@ -0,0 +1,79 @@
+package ru.art3m4ik3.kraan.presentation.pages
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import ru.art3m4ik3.kraan.R
+import ru.art3m4ik3.kraan.databinding.FragmentMainScreenBinding
+
+class MainScreenFragment : Fragment() {
+    private lateinit var binding: FragmentMainScreenBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainScreenBinding.inflate(inflater, container, false)
+
+        setCompanyInfo()
+        setFeatures(
+            listOf(
+                mapOf("name" to "this is a name1", "description" to "this is a description1"),
+                mapOf("name" to "this is a name2", "description" to "this is a description2"),
+                mapOf("name" to "this is a name3", "description" to "this is a description3")
+            )
+        )
+
+        return binding.root
+    }
+
+    private fun setCompanyInfo() {
+        view.apply {
+            // TODO: get info from api
+
+            binding.companyNameTextView.text = "Kraan"
+            binding.companyInfoTextView.text =
+                "Kraan - ведущая компания по продаже мобильных кранов. Мы предлагаем широкий выбор колесных кранов грузоподъёемностью от 16 до 300 тонн для различных строительных и промышленных задач."
+        }
+    }
+
+    private fun setFeatures(features: List<Map<String, String>>) {
+        view.apply {
+            // TODO: get info from api
+
+            features.forEach { map ->
+                val cardView = LayoutInflater.from(context)
+                    .inflate(R.layout.feature_card, binding.featuresContainer, false) as CardView
+
+                map.forEach { (key, value) ->
+                    Log.i(key, value)
+
+                    when (key) {
+                        "name" -> {
+                            cardView.findViewById<TextView>(R.id.featureTitleTextView).text = value
+                        }
+
+                        "description" -> {
+                            cardView.findViewById<TextView>(R.id.featureDescriptionTextView).text =
+                                value
+                        }
+                    }
+                }
+
+                val layoutParams = LinearLayout.LayoutParams(
+                    resources.getDimensionPixelSize(R.dimen.feature_card_width),
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                layoutParams.marginEnd =
+                    resources.getDimensionPixelSize(R.dimen.feature_card_margin)
+                cardView.layoutParams = layoutParams
+
+                binding.featuresContainer.addView(cardView)
+            }
+        }
+    }
+}
